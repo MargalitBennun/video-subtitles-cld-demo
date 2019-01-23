f398a492ed6caae4a9fffa72af34ce6bab228010
margalitb@gmail.com <margalitb@liveperson.com>
2019-01-23 17:09:08 +0200

    video player

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video-player msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VideoPlayer from './components/VideoPlayer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      VideoPlayer
   }
 }
 </script>
